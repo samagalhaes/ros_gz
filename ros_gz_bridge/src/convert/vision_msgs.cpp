588279122c660a3b3b7e258bbd5331f8f9de5fce
Sandro Magalhães <sandro.a.magalhaes@inesctec.pt>
2022-09-02 14:45:08 +0100

    Bridging 2D bounding boxes
    Signed-off-by: Sandro Magalhães <sandro.a.magalhaes@inesctec.pt>

--- ros_gz_bridge/src/convert/vision_msgs.cpp
+++ ros_gz_bridge/src/convert/vision_msgs.cpp
@@ -70,7 +70,6 @@ convert_gz_to_ros(
   ros_msg.results.push_back(object);
 }
 
-// TODO: Change this
 template<>
 void
 convert_ros_to_gz(
@@ -95,6 +94,94 @@ convert_gz_to_ros(
   for(auto& gz_box : gz_msg.annotated_box()) {
     auto& ros_box = ros_msg.detections.emplace_back(); 
     convert_gz_to_ros(gz_box, ros_box);
+  }
+}
+
+template<>
+void
+convert_ros_to_gz(
+  const vision_msgs::msg::BoundingBox2D & ros_msg,
+  ignition::msgs::AxisAligned2DBox & gz_msg)
+{
+  auto ros_center = ros_msg.center.position;
+
+  auto gz_min_corner = gz_msg.mutable_min_corner();
+  auto gz_max_corner = gz_msg.mutable_max_corner();
+
+  gz_min_corner->set_x(ros_center.x - ros_msg.size_x / 2.0);
+  gz_min_corner->set_y(ros_center.x - ros_msg.size_x / 2.0);
+
+  gz_max_corner->set_x(ros_center.x + ros_msg.size_x / 2.0);
+  gz_max_corner->set_y(ros_center.x + ros_msg.size_x / 2.0);
+}
+
+template<>
+void
+convert_gz_to_ros(
+  const ignition::msgs::AxisAligned2DBox & gz_msg,
+  vision_msgs::msg::BoundingBox2D & ros_msg)
+{
+  auto min_corner = gz_msg.min_corner();
+  auto max_corner = gz_msg.max_corner();
+  ros_msg.center.position.x = min_corner.x() + (max_corner.x() - min_corner.x()) / 2.0;
+  ros_msg.center.position.y = min_corner.y() + (max_corner.y() - min_corner.y()) / 2.0;
+  ros_msg.center.theta = 0.0;
+
+  ros_msg.size_x = max_corner.x() - min_corner.x();
+  ros_msg.size_y = max_corner.y() - min_corner.y();
+}
+
+template<>
+void
+convert_ros_to_gz(
+  const vision_msgs::msg::Detection2D & ros_msg,
+  ignition::msgs::AnnotatedAxisAligned2DBox & gz_msg)
+{
+  convert_ros_to_gz(ros_msg.header, (*gz_msg.mutable_header()));
+  convert_ros_to_gz(ros_msg.bbox, (*gz_msg.mutable_box()));
+
+  auto result = ros_msg.results.cbegin();
+  if (result != ros_msg.results.end())
+    gz_msg.set_label(std::stoi(result->hypothesis.class_id));
+}
+
+template<>
+void
+convert_gz_to_ros(
+  const ignition::msgs::AnnotatedAxisAligned2DBox & gz_msg,
+  vision_msgs::msg::Detection2D & ros_msg)
+{
+  convert_gz_to_ros(gz_msg.header(), ros_msg.header);
+  convert_gz_to_ros(gz_msg.box(), ros_msg.bbox);
+
+  auto& result = ros_msg.results.emplace_back();
+  result.hypothesis.class_id = std::to_string(gz_msg.label());
+  result.hypothesis.score = 1.0;
+}
+
+template<>
+void
+convert_ros_to_gz(
+  const vision_msgs::msg::Detection2DArray & ros_msg,
+  ignition::msgs::AnnotatedAxisAligned2DBox_V & gz_msg)
+{
+  convert_ros_to_gz(ros_msg.header, (*gz_msg.mutable_header()));
+  for (auto& ros_detection : ros_msg.detections){
+    ignition::msgs::AnnotatedAxisAligned2DBox* gz_detection = gz_msg.add_annotated_box();
+    convert_ros_to_gz(ros_detection, (*gz_detection));
+  }
+}
+
+template<>
+void
+convert_gz_to_ros(
+  const ignition::msgs::AnnotatedAxisAligned2DBox_V & gz_msg,
+  vision_msgs::msg::Detection2DArray & ros_msg)
+{
+  convert_gz_to_ros(gz_msg.header(), ros_msg.header);
+  for (auto& gz_detection : gz_msg.annotated_box()){
+    auto& ros_detection = ros_msg.detections.emplace_back();
+    convert_gz_to_ros(gz_detection, ros_detection);
   }
 }
 
